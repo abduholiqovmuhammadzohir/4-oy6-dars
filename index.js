10fe7d91306a93886295a2f404d4c21cec05597a
abduholiqovmuhammadzohir <abduholiqovmuhammadzohir@gmail.com>
2023-12-06 13:45:15 +0500

    homework

---
+++ index.js
@@ -0,0 +1,274 @@
+
+// for (let i = 1; i <= 10; i++) {
+//     let str = '';
+//     for (let j = 1; j <= i; j++){
+//         str += j
+//     }
+//     console.log(str);
+// }
+
+// ---------------------------------------------
+  
+// let i = 1
+// let n = 20
+// let sum = 0;
+// while (i <= n) {
+//     if (n % i == 0) {
+//         sum++;
+//     }
+//     i++
+// }
+// console.log(sum);
+
+// ---------------------------------------------------
+
+// let i = 1
+// let n = 20
+// let sum = 0;
+// while (i <= n) {
+//     if (n % i == 0) {
+//         sum += i;
+//     }
+//     i++
+// }
+// console.log(sum);
+
+// ---------------------------------------------------
+
+// let n = +prompt("Oyning rqamini kiriting");
+// let res;
+// switch (n) {
+//     case 1:
+//         res = "Yanvar";
+//         break;
+//     case 2:
+//         res = "Fevral";
+//         break;
+//     case 3:
+//         res = "Mart";
+//         break;
+//     case 4:
+//         res = "A prel";
+//         break;
+//     case 5:
+//         res = "May";
+//         break;
+//     case 6:
+//         res = "Iyun";
+//         break;
+//     case 7:
+//         res = "Iyul";
+//         break;
+//     case 8:
+//         res = "Avgust";
+//         break;
+//     case 9:
+//         res = "Sentyabr";
+//         break;
+//     case 10:
+//         res = "Oktyabr";
+//         break;
+//     case 11:
+//         res = "Noyabr";
+//         break;
+//     case 12:
+//         res = "Dekabr";
+//         break;
+
+//     default:
+//         res = "Bundy oy kuni mavjut emas";
+//         break;
+// }
+
+// console.log(res);
+
+// 1 - misol
+
+// let i = 1
+// let n = 10
+// let sum = 0;
+// while (i <= n) {
+//     if (i % 2 == 0) {
+//         sum += i;
+//     }
+//     i++
+// }
+// console.log(sum);
+
+// 2 - misol
+
+// let i = 1
+// let n = 10
+// let sum = 0;
+// while (i <= n) {
+//     if (i % 3 == 0) {
+//         sum += i;
+//     }
+//     i++
+// }
+// console.log(sum);
+
+// 3 - misol
+
+// let i = 1
+// let n = 22
+// let sum = 0;
+// while (i <= n) {
+//     if (i % 3 == 0 && i % 7 == 0) {
+//         sum++
+//     }
+//     i++
+// }
+// console.log(sum);
+
+
+// 4 - misol
+
+// let i = 1;
+// let n = 11;
+// let sum  = 0;
+// let res;
+// while (i <= n) {
+//     if (n % i == 0) {
+//         sum ++;
+//     }if (sum <= 2) {
+//         res = "tub son"
+//     }else{
+//         res = "tub emas"
+//     }
+//     i++
+// }
+// console.log(res);
+
+
+// 5 - misol
+
+let i = 1;
+let n = 22;
+
+let sum = 0;
+
+while (i <= n) {
+    sum += 1;
+    i++ ;
+}
+console.log(sum);
+
+// 6 - misol
+
+// let n = 123
+// let  res = '';
+
+// let b = n % 10;
+// let o = Math.trunc(n / 10) % 10;
+// let y = Math.trunc(n / 100);
+
+// switch (y) {
+//     case 1:
+//         res = "bir yuzi "
+//         break;
+//     case 2:
+//         res = "ikki yuzi "
+//         break;
+//     case 3:
+//         res = "uch yuzi "
+//         break;
+//     case 4:
+//         res = "to'rt yuzi "
+//         break;
+//     case 5:
+//         res = "besh yuzi "
+//         break;
+//     case 6:
+//         res = "olti yuzi "
+//         break;
+//     case 7:
+//         res = "yetti yuzi "
+//         break;
+//     case 8:
+//         res = "sakkiz yuzi "
+//         break;
+//     case 9:
+//         res = "to'riz yuzi "
+//         break;
+
+//     default:
+//         res = "Bunday son mavjud emas"
+//         break;
+
+// }
+// switch (o) {
+//     case 1:
+//         res += "o'n "
+//         break;
+//     case 2:
+//         res += "yigirma "
+//         break;
+//     case 3:
+//         res += "o'ttiz "
+//         break;
+//     case 4:
+//         res += "qiriq "
+//         break;
+//     case 5:
+//         res += "elik "
+//         break;
+//     case 6:
+//         res += "oltmish "
+//         break;
+//     case 7:
+//         res += "yetmish "
+//         break;
+//     case 8:
+//         res += "sakson "
+//         break;
+//     case 9:
+//         res += "to'hson "
+//         break;
+
+//     default:
+//         res = "Bunday son mavjud emas"
+//         break;
+
+// }
+// switch (b) {
+//     case 1:
+//         res += "bir"
+//         break;
+//     case 2:
+//         res += "ikki"
+//         break;
+//     case 3:
+//         res += "uch"
+//         break;
+//     case 4:
+//         res += "to'rt"
+//         break;
+//     case 5:
+//         res += "besh"
+//         break;
+//     case 6:
+//         res += "olti"
+//         break;
+//     case 7:
+//         res += "yeti"
+//         break;
+//     case 8:
+//         res += "sakkiz"
+//         break;
+//     case 9:
+//         res += "to'qiz"
+//         break;
+
+//     default:
+//         res = "Bunday son mavjud emas"
+//         break;
+
+// }
+
+// console.log(res);
+
+
+// 7 - misol
+
+
